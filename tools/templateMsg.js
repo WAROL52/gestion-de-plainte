5fc8754ed68f545d474413ecd5d84fb5664c6d47
rabe52 <119914443+rabe52@users.noreply.github.com>
2022-12-06 04:58:08 +0000

    version rabe

--- tools/templateMsg.js
+++ tools/templateMsg.js
@@ -1,8 +1,7 @@
 import ejs from "ejs"
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
-import path, { dirname } from "path";
+import path, { dirname, join } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url)); 
 
-
-export const templateEmail = ejs.compile(readFileSync(__dirname+"\\msg.ejs",{encoding:"utf-8"}));
+export const templateEmail = ejs.compile(readFileSync(join(__dirname,"msg.ejs"),{encoding:"utf-8"}));
